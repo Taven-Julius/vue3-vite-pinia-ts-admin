91368c9ca1ed632008d6d23169d39c80e0af2705
Julius <julius.wang@cdpgroupltd.com>
2022-08-01 18:01:47 +0800

    初始化仓库

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="wrapper">
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>
+          <RouterView />
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+
+
+</template>
+
+<style scoped lang="scss">
+  .wrapper {
+    min-height: 100vh;
+    width: 100%;
+    .el-aside {
+      background: #f5f5f5;
+      text-align: center;
+    }
+    .el-header {
+      background: #c6e2ff;
+    }
+  }
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<style lang="scss">
+  .about {
+    min-height: calc(100vh - 100px);
+    display: flex;
+    align-items: center;
+  }
+</style>
